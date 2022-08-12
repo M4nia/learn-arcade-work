aeff2f1e6165929d3c6e8dc7a02eafb09de6a3fa
M4nia <97772455+M4nia@users.noreply.github.com>
2022-08-12 13:25:42 -0700

    finished lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,9 @@
-test
+print("Let's see what I learned/reviewed today.")
+print("I can use backslash to insert quotes like this \" without raising an error.")
+print("I can use \\n to move to new lines\nlike this.")
+print("""I can also use triple
+quotes to write on
+new lines.
+In addition, escape code/characters allow me to print
+otherwise unprintable characters.
+\\t creates tabs\tlike this.""")
